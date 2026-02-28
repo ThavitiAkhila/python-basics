118cd725d4323a17e64faae2b048604b394af723
ThavitiAkhila <thavitiakhila0420@gmail.com>
2026-02-28 22:13:40 +0530

    Update ifprogram.py

--- ifprogram.py
+++ ifprogram.py
@@ -1,3 +1,16 @@
-n=int(input())
-for i in range(1,n+1):
-    print('*',*i)
+marks=int(input())
+if marks >= 0 && marks <= 100:
+    if marks > 90:
+        print("Out standing")
+    elif marks >= 80:
+        print("Distinction")
+    elif marks >= 70:
+        print("First class")
+    elif marks >= 60:
+        print("Second class")
+    elif marks >= 50:
+        print("Third class")
+    else:
+        print("Fail")
+else:
+    print("Invalid marks entered")
