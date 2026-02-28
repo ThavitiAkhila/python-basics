15ec0a1c3c439764ff830e1546f98b985420aeba
ThavitiAkhila <thavitiakhila0420@gmail.com>
2026-02-28 22:21:42 +0530

    Deleted code

--- ifprogram.py
+++ ifprogram.py
@@ -1,16 +1,1 @@
-marks=int(input())
-if marks >= 0 && marks <= 100:
-    if marks > 90:
-        print("Out standing")
-    elif marks >= 80:
-        print("Distinction")
-    elif marks >= 70:
-        print("First class")
-    elif marks >= 60:
-        print("Second class")
-    elif marks >= 50:
-        print("Third class")
-    else:
-        print("Fail")
-else:
-    print("Invalid marks entered")
+
